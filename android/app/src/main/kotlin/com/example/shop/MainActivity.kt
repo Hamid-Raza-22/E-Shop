910f299a0da45327a88727aa4f6454f62975ca99
Hamid Raza <hamidraza.engr@gmail.com>
2026-08-20 17:00:45 +0500

    setup Firebase and update application IDs

--- android/app/src/main/kotlin/com/example/shop/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shop/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.shop
+package com.hamid.gogguz
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shop/route/route_constants.dart';
 import 'package:shop/route/router.dart' as router;
 import 'package:shop/theme/app_theme.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
